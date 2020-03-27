55d5f545c19841e184548d6a9594f4d27311b064
Alex Benasutti <benasua1@tcnj.edu>
2020-03-27 01:36:22 -0400

    Add more code documentation

--- src/Nutrify/Nutrify/SurveyQuestionsViewController.swift
+++ src/Nutrify/Nutrify/SurveyQuestionsViewController.swift
@@ -11,17 +11,19 @@ import UIKit
 // SurveyQuestionsViewController: View controller for all user identification survey questionnaire questions and functionality. Will redirect to homepage after completion.
 class SurveyQuestionsViewController: UIViewController {
     
-    @IBOutlet weak var genderField: UITextField!
-    @IBOutlet weak var dateOfBirthField: UIDatePicker!
-    @IBOutlet weak var heightField: UITextField!
-    @IBOutlet weak var weightField: UITextField!
-    @IBOutlet weak var goalWeightOverallField: UITextField!
-    @IBOutlet weak var goalWeightWeeklyField: UITextField!
-    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var genderField: UITextField!            // gender text field
+    @IBOutlet weak var dateOfBirthField: UIDatePicker!      // DOB text field
+    @IBOutlet weak var heightField: UITextField!            // height text field
+    @IBOutlet weak var weightField: UITextField!            // weight text field
+    @IBOutlet weak var goalWeightOverallField: UITextField! // goal weight text field
+    @IBOutlet weak var goalWeightWeeklyField: UITextField!  // goal weekly weight text field
+    @IBOutlet weak var textView: UITextView!                // results text view
         
+    // viewDidLoad: Active when survey questions page loads for the first time
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Assign all text fields as delegates
         genderField.delegate = self
         heightField.delegate = self
         weightField.delegate = self
@@ -29,6 +31,8 @@ class SurveyQuestionsViewController: UIViewController {
         goalWeightWeeklyField.delegate = self
     }
 
+    // enterAnswers: Activated by "Done" button on survey. Sends results to textView and navigates user to home page
+    // input: Button sender
     @IBAction func enterAnswers(_ sender: Any)
     {
         textView.text = "Gender: \(genderField.text!)\nHeight: \(heightField.text!) in\nWeight: \(weightField.text!)\nGoal: \(goalWeightOverallField.text!)\nWeekly: \(goalWeightWeeklyField.text!)"
@@ -36,6 +40,7 @@ class SurveyQuestionsViewController: UIViewController {
         navigateToHome()
     }
     
+    // navigateToHome: navigate user to home page view controller
     private func navigateToHome()
     {
         let vc = storyboard?.instantiateViewController(identifier: "home") as! HomeViewController
@@ -43,6 +48,10 @@ class SurveyQuestionsViewController: UIViewController {
         present(vc, animated: true)
     }
     
+    // touchesBegan: Check for touches when certain input text fields (i.e. number pads) are open. Close when user touches outside of text field
+    // inputs
+    //   touches: Set of UITouch instances to represent the touches for the starting phase of an event
+    //   event: The event to which the touches belong
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         heightField.resignFirstResponder()
         weightField.resignFirstResponder()
@@ -53,6 +62,9 @@ class SurveyQuestionsViewController: UIViewController {
 
 extension SurveyQuestionsViewController : UITextFieldDelegate
 {
+    // textFieldShouldReturn: asks a delegate if the text field should process the pressing of the return button. Relinquish the status of first responder to the text field
+    // input
+    //   textField: the text field whose return button was pressed
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
